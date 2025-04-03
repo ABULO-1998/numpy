1bd4371a981aaede409b4a4286131e0d01cca29e
ABULO-1998 <adongoabulo@gmail.com>
2025-04-03 22:31:53 +0300

    speed test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 
 # a = np.array([1,2,3])
@@ -45,9 +46,45 @@ import numpy as np
 # dot=a1@a2
 # print(dot)
 
-a=np.arange(1,7)
-print(a)
-print(a.shape)
+# a=np.arange(1,7)
+# print(a)
+# print(a.shape)
 
-b=a.reshape(2,3)
-print(b)
+# b=a.reshape(2,3)
+# print(b)
+
+from timeit import default_timer as timer
+
+a = np.random.randn(1000)
+b = np.random.randn(1000)
+
+A = list(a)
+B = list(b)
+
+T =1000
+
+def dot1():
+    dot = 0
+    for i in range(len(A)):
+        dot +=A[i]*B[i]
+    return(dot)
+
+def dot2():
+    return np.dot(a,b)
+
+start = timer()
+for t in range(T):
+    dot1()
+end = timer()
+t1=end-start
+
+start = timer()
+for t in range(T):
+    dot2()
+end = timer()
+t2=end-start
+
+print('list calculation',t1)
+print('np.dot',t2)
+print('ratio',t1/t2)
+
