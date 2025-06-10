c8190aa478d0801eee8a07762beda5bf92728073
ABULO-1998 <adongoabulo@gmail.com>
2025-06-10 16:28:56 +0300

    numpy

--- nd.py
+++ nd.py
@@ -34,3 +34,11 @@ b=np.array([[12,13,14,15]])
 c=np.hstack((a,b))
 print(c)
 
+a = np.random.random((3,2))
+print (a)
+
+a = np.random.randn(1000)
+print(a.mean(), a.var())
+
+a = np.random.randint(3, 10, size=(3,3))
+print (a)
